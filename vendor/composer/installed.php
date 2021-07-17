a5f2b665af8a99793bff5c0f09259dedd3b3958e
jjenus <biz.jenus@gmail.com>
2021-07-17 20:22:18 +0100

    adding vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '11f5e95adb126a1da2f75d2d5fd7d2ac233ac3a3',
+    'name' => 'jjenus/buddie',
+  ),
+  'versions' => 
+  array (
+    'abraham/twitteroauth' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a424e80a1200674451844fbaae8a0098a316a01',
+    ),
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.2.10',
+      'version' => '1.2.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9fdb22c2e97a614657716178093cd1da90a64aa8',
+    ),
+    'jjenus/buddie' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '11f5e95adb126a1da2f75d2d5fd7d2ac233ac3a3',
+    ),
+    'symfony/http-foundation' => 
+    array (
+      'pretty_version' => 'v3.4.47',
+      'version' => '3.4.47.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9885fcce6fe494201da4f70a9309770e9d13dc8',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2df51500adbaebdc4c38dea4c89a2e131c45c8a1',
+    ),
+    'symfony/polyfill-php70' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f03a781d984aae42cebd18e7912fa80f02ee644',
+    ),
+    'tonyhhyip/sse' => 
+    array (
+      'pretty_version' => '3.0.0-beta',
+      'version' => '3.0.0.0-beta',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '86f79a14dd2d4634b78bd3bbbc236614a994292e',
+    ),
+  ),
+);
